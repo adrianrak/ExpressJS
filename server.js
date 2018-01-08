9ee71edef8bfc0dcdb555ced814bf51d95d8bb3d
Adrian Rak <adrrak@tlen.pl>
2018-01-08 20:20:52 +0100

    static

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
-// const fs = require('fs');
 
 const app = express();
-// let stringifyFile;
 
-// app.use(bodyParser.json());
 app.use(express.static('assets'));
 app.get('/', function(req, res) {
     res.sendFile('/index.html');
