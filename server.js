0f6f5ea4e7a736dc0c34749ce9ac7a9be53d1866
Adrian Rak <adrrak@tlen.pl>
2018-01-09 10:09:23 +0100

    middleware

--- server.js
+++ server.js
@@ -1,26 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const fs = require('fs');
 
 const app = express();
-let stringifyFile;
 
-app.use(bodyParser.json());
-
-app.get('/getNote', function(req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        stringifyFile = data;
-        res.send(data);
-    });   
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store.');
+    next();
 });
 
-app.post('/updateNote/:note', function(req, res) {
-    stringifyFile += req.params.note;
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-        if (err) throw err;
-        console.log('file updated');
-    });
+app.get('/', function(req, res) {
+    res.send('Hello world');   
+});
+
+app.get('/store', function(req, res) {
+    res.send('To jest sklep.');   
 });
 
 let server = app.listen(3000, function() {
