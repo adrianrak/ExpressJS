7dccc65b306016015b93461818e6d744130c16f4
Adrian Rak <adrrak@tlen.pl>
2018-01-10 20:47:29 +0100

    google

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '825522026083-ja5noa432ip90koi7gd8djbg3lifms0u.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'k5D1X-8LBzQJk2w_vOIOshf-',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,40 +1,59 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
 const app = express();
+let googleProfile = {};
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+function(accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+    cb(null, profile);
+}
+));
 
 app.set('view engine', 'pug');
 app.set('views','./views');
-app.use(express.static('assets'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/store', function(req, res, next){
-    console.log('Jestem pośrednikiem przy żądaniu do /store.');
-    next();
-});
+//app routes
 
 app.get('/', function(req, res) {
-    res.send('Hello world');   
+    res.render('index', { user: req.user });   
 });
 
-app.get('/store', function(req, res) {
-    res.send('To jest sklep.');   
+app.get('/logged', function(req, res){
+    res.render('logged', { user: googleProfile });
 });
 
-app.get('/first-template', function(req, res){
-    res.render('first-template');
-});
+//Passport routes
 
-app.get('/second-template', function (req, res) {
-    res.render('second-template');
-    // const response = {
-    //     first_name: req.query.first_name,
-    //     last_name: req.query.last_name
-    // };
-    // res.end(JSON.stringify(response));
-});
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
 
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
 
 let server = app.listen(3000, function() {
     console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
-});
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
