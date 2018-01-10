78980e89b239161f5fae5da542708e3578855718
Adrian Rak <adrrak@tlen.pl>
2018-01-10 14:01:00 +0100

    templates

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+app.use(express.static('assets'));
 
 app.use('/store', function(req, res, next){
     console.log('Jestem pośrednikiem przy żądaniu do /store.');
@@ -15,7 +18,23 @@ app.get('/store', function(req, res) {
     res.send('To jest sklep.');   
 });
 
+app.get('/first-template', function(req, res){
+    res.render('first-template');
+});
+
+app.get('/second-template', function (req, res) {
+    res.render('second-template');
+    // const response = {
+    //     first_name: req.query.first_name,
+    //     last_name: req.query.last_name
+    // };
+    // res.end(JSON.stringify(response));
+});
+
+
 let server = app.listen(3000, function() {
     console.log('Przykładowa aplikacja nasłuchuje na http://localhost:3000');
 });
-
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
