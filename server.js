3bb25a673fc93baca93b48406e1fb20bd9b54d64
Adrian Rak <adrrak@tlen.pl>
2018-01-11 12:12:02 +0100

    authUser

--- server.js
+++ server.js
@@ -2,21 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use('/store', function(req, res, next){
+const authUser = (req, res, next) => {
     console.log('Autoryzacja użytkownika...');
     next();
-});
+};
 
-app.use('/store', function(req, res, next){
+const checkPermission = (req, res, next) => {
     console.log('Sprawdzanie uprawnień.');
     next();
-});
+};
 
 app.get('/', function(req, res) {
     res.send('Hello world');   
 });
 
-app.get('/store', function(req, res) {
+app.get('/store', authUser, checkPermission, function(req, res) {
     res.send('To jest sklep.');   
 });
 
